583e5edd7ad5e20282379e87d38d9e11375d921d
Misael Landero <francisco.landero@dexcom.com>
2024-10-07 15:26:11 -0700

    Initial Commit

---
+++ BootCampApp/BootCampAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BootCampAppApp.swift
+//  BootCampApp
+//
+//  Created by Francisco Landero on 07/10/24.
+//
+
+import SwiftUI
+
+@main
+struct BootCampAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
