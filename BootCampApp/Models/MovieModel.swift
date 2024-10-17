9bf72e7d767c7202c13448359b7628db9c4559bb
Ines Pacheco <ip0421@dexcom.com>
2024-10-16 18:09:12 -0700

    BCA: Add mock response and set movie model

--- BootCampApp/Models/MovieModel.swift
+++ BootCampApp/Models/MovieModel.swift
@@ -1,5 +1,5 @@
 //
-//  MovieModel.swift
+//  Movie.swift
 //  BootCampApp
 //
 //  Created by Francisco Landero on 16/10/24.
@@ -7,33 +7,21 @@
 
 import SwiftUI
 
-// MARK: - MovieModelElement
+// MARK: - Movie
 struct Movie: Codable, Identifiable {
-    let id: String
+    let id: Int
     let title: String
     let posterPath: String?
     let releaseDate: String?
-    /*
     let adult: Bool?
     let popularity: Double?
-    let overview : String?
     let revenue : Int?
-    
-    let voteAverage: Double?
-    let voteCount: Int?
-    let status: Status?
-    let revenue, runtime: Int?
-   
-    let backdropPath: String?
     let budget: Int?
-    let homepage: String?
     let imdbID: String?
-    let originalLanguage: String?
-    let originalTitle, overview: String
-    //   let tagline, genres, productionCompanies, productionCountries: String?
-    let spokenLanguages: String?*/
+    let overview : String?
+    let tagline: String?
+    let genres: String?
     
-    //Computed properity
     var imageURL: URL? {
          URL(string: posterPath ?? "")
     }
@@ -43,36 +31,7 @@ struct Movie: Codable, Identifiable {
         case title
         case posterPath = "poster_path"
         case releaseDate = "release_date"
-        //case adult, popularity, overview,revenue
-        /*
-        case voteAverage = "vote_average"
-        case voteCount = "vote_count"
-        case status
-        case revenue, runtime, adult
-        case backdropPath = "backdrop_path"
-        case budget, homepage
+        case adult, popularity, overview,revenue, budget, tagline, genres
         case imdbID = "imdb_id"
-        case originalLanguage = "original_language"
-        case originalTitle = "original_title"
-       // case overview, popularity
-        case tagline, genres
-        case productionCompanies = "production_companies"
-        case productionCountries = "production_countries"
-        case spokenLanguages = "spoken_languages"*/
     }
 }
-/*
-enum OriginalLanguage: String, Codable {
-    case en = "en"
-    case es = "es"
-}*/
-
-enum Status: String, Codable {
-    case released = "Released"
-}
-
- 
-struct MovieResponse: Codable {
-    let results: [Movie]
-}
- 
