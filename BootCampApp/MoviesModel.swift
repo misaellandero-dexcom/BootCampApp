1d9ea2af5ca4c44b293acc2e53e35ecf7387a0f9
Misael Landero <francisco.landero@dexcom.com>
2024-10-21 14:17:47 -0700

    Update MoviesModel.swift
    Remove api calls

--- BootCampApp/MoviesModel.swift
+++ BootCampApp/MoviesModel.swift
@@ -10,17 +10,22 @@ import SwiftUI
 @Observable class MoviesModel {
     
     var movies = [Movie]()
-    private let service = MoviesService()
     
     func fetchMovies() {
-        Task {
-            movies = await service.fetchMovies()
-        }
+        featchMockMovies()
     }
     
     func searchMovieByTitle(title : String) {
-        Task {
-            movies = await service.fetchMovies(title: title)
-        }
+        featchMockMovies()
+    }
+    
+    func featchMockMovies(){
+        let moviesData = Data(mockResponse.utf8)
+               
+               do {
+                   movies = try JSONDecoder().decode([Movie].self, from: moviesData)
+               } catch {
+                   print("Failed to decode mock data: \(error)")
+               }
     }
 }
